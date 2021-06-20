538f1fb4db02543c6b85a3a1dedba7a3dd6c4da3
AVINASH PUJARI <pujariavinash97@gmail.com>
2021-06-20 19:42:51 +0530

    TESTINT

---
+++ TESTINT.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+#ifndef ONLINE_JUDGE
+freopen("input.txt", "r", stdin);
+freopen("output.txt", "w", stdout);
+#endif
+
+int x, y = 0;
+cin >> x;
+cin >> y;
+cout << x + y;
+
+return 0;
+}
